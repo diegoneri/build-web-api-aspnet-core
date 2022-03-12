747b7fbfe6f0641e0700466cbfc3b9eb212e16ca
Diego Neri <diego@diegoneri.com>
2022-03-12 16:28:55 -0300

    Complete the tutorial with CRUD operations

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,70 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll()
+    {
+        var pizzas = PizzaService.GetAll();
+        return pizzas.Count() > 0 ? pizzas : NotFound();
+    }
+
+
+    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
+    // POST action
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+    }
+    // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+            return BadRequest();
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
+    // DELETE action
+[HttpDelete("{id}")]
+public IActionResult Delete(int id)
+{
+    var pizza = PizzaService.Get(id);
+   
+    if (pizza is null)
+        return NotFound();
+       
+    PizzaService.Delete(id);
+   
+    return NoContent();
+}    
+}
